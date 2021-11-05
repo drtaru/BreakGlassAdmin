0688f0810bf095f6b6f5824de8b0153ac2201096
Chad Lawson <cdlawson@coldshadow.com>
2021-11-05 10:37:19 -0500

    Added two new password schemes, 'wopr' and 'custom' to the mix.

--- breakglassAdmin.sh
+++ breakglassAdmin.sh
@@ -1,41 +1,64 @@
 #!/bin/bash
 
-################################
-##/////////////////////)######## Break Glass Admin
-##|                      )######
-##|                        )#### Creates and/or changes the password of a
-##|                         )### backdoor admin account for emergency use
-##|         #######)         )##
-##|         ########)        )## TYPE: Jamf Policy Script
-##|         #######)        )###
-##|                         )### Parameters:
-##|                       )##### $1-3 - Reserved by Jamf
-##|                      )###### $4 - Username of admin account
-##|      |  ####\         \##### $5 -
-##|      |  #####\         \#### $6 - Password generation method (see code)
-##|    | |  ######\         \### $7 - Storage method (see code)
-##|  | | |  #######\         \## $8 - Extension Attribute for password storage
-################################ $9 - Force (y/n) (see code)
-##
-## Latest version and additional notes available at our GitHub
-##      https://github.com/Rocketman-Tech/BreakGlassAdmin
-##
-################################################################################
+: HEADER = <<'EOL'
+
+██████╗  ██████╗  ██████╗██╗  ██╗███████╗████████╗███╗   ███╗ █████╗ ███╗   ██╗
+██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝████╗ ████║██╔══██╗████╗  ██║
+██████╔╝██║   ██║██║     █████╔╝ █████╗     ██║   ██╔████╔██║███████║██╔██╗ ██║
+██╔══██╗██║   ██║██║     ██╔═██╗ ██╔══╝     ██║   ██║╚██╔╝██║██╔══██║██║╚██╗██║
+██║  ██║╚██████╔╝╚██████╗██║  ██╗███████╗   ██║   ██║ ╚═╝ ██║██║  ██║██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+        Name: Break Glass Admin
+ Description: Creates/manages a hidden admin account with a random password
+  Parameters: $1-$3 - Reserved by Jamf (Mount Point, Computer Name, Username)
+              ADMINUSER - The name of the admin account
+							PASSMODE - Which method of creating a password to use (see below)
+							FORCE - Force (0 or 1) - If password is unknown,
+							        do we delete the old account and recreate?
+
+							DEFAULTPW - Password to try if previous password is unknown
+
+							STORAGE - "Local," "Server," or "Both"
+							APIHASH - Base64 encoded "user:password" of API user for
+							          server-side storage
+							EXTATTR - Name of extension attribute where password is stored
+								(e.g. "Backdoor Admin Password" for server-side or
+								      "tech.rocketman.breakglass.plist" for local)
+
+Available Password Methods:
+	          'nato' - Combines words from the NATO phonetic alphabet
+	                   (e.g. "WhiskeyTangoFoxtrot")
+						'wopr' - Like the launch codes in the 80s movie, "Wargames"
+                     (https://www.imdb.com/title/tt0086567)
+										 (e.g. "CPE 1704 TKS")
+	          'xkcd' - Using the system from the XKCD webcomic
+						         (https://xkcd.com/936)
+	         'names' - Same as above but only with the propernames database
+  	'pseudoRandom' - Based on University of Nebraska' LAPS system
+	                   (https://github.com/NU-ITS/LAPSforMac)
+'custom' (default) - Customizable format with the following defaults
+                     * 16 characters
+										 * 1 Upper case character (min)
+										 * 1 Lower case character (min)
+										 * 1 Digit (min)
+										 * 1 Special character (min)
+
+Latest version and additional notes available at our GitHub
+		https://github.com/Rocketman-Tech/BreakGlassAdmin
+
+EOL
 
 ## Get the policy variables
-ADMINUSER="$4"  ## What is the username of the admin user to change/create. Also used for real name.
-PASSMODE="$6"   ## Which method to use to create the password (nato, xkcd, names, pseudoRandom)
-DEFAULTPW=""    ## Password to try if old password is unknown
-FORCE="$9"      ## 1 (true) or 0 (false) - If true and old password is unknown or can't be changed,
-                ##		the script will delete the account and re-create it instead.
-                ##		USE WITH EXTREME CAUTION!
-APIHASH=""      ## Base64 encoded "user:password" of API user for server-side storage
-
-STORAGE="$7"    ## "LOCAL" or Base64 encoded "user:password" string
-
-EXTATTR="$8"    ## Name of the extension attribute where password is stored
-                ##		(e.g. "Backdoor Admin Password" for cloud or "tech.rocketman.backdooradmin.plist" for local)
-
+ADMINUSER="$4" 	## What is the name of the admin user to change/create
+ADMINFULL="$5" 	## Full name of admin user
+PASSMODE="$6"	## Which method to use to create the password (nato, xkcd, names, pseudoRandom)
+STORAGE="$7" 	## "LOCAL" or Base64 encoded "user:password" string
+EXTATTR="$8" 	## Name of the extension attribute where password is stored
+				##	(e.g. "Backdoor Admin Password" for cloud or "tech.rocketman.backdooradmin.plist" for local)
+FORCE="$9"		## 1 (true) or 0 (false) - If true and old password is unknown or can't be changed,
+				##	the script will delete the account and re-create it instead.
+				##	USE WITH EXTREME CAUTION!
 
 ## Set additional variables based on local or remote storage
 if [[ ${STORAGE} == "LOCAL" ]]; then
@@ -47,9 +70,9 @@ else
 	SERIAL=$(system_profiler SPHardwareDataType | grep -i serial | grep system | awk '{print $NF}')
 fi
 
-#################
-### FUNCTIONS ###
-#################
+##
+## Functions
+##
 
 function debugLog () {
 	message=$1
@@ -68,6 +91,13 @@ function createRandomPassword() {
 			NATO=(Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey Yankee Zulu)
 			MAX=${#NATO[@]}
 			NEWPASS=$(for u in $(jot -r ${NUM} 0 $((${MAX}-1)) ); do  echo -n ${NATO[$u]} ; done)
+			;;
+
+		wopr) ## Like the launch codes in the 80s movie "Wargames" (e.g. "CPE 1704 TKS")
+			PRE=$(jot -nrc -s '' 3 65 90)
+			NUM=$(jot -nr -s '' 4 0 9)
+			POST=$(jot -nrc -s '' 3 65 90)
+			NEWPASS="${PRE} ${NUM} ${POST}"
 			;;
 
 		xkcd) ## Using the system from the XKCD webcomic (https://xkcd.com/936/)
@@ -90,12 +120,62 @@ function createRandomPassword() {
 			NEWPASS=$(echo "${CHOICES}" | tr -d "[:space:]" )
 			;;
 
-		schema) ## Get all the attributes from an XML blob
-			;;
-
-		pseudoRandom | *) ## Based on University of Nebraska' LAPS system (https://github.com/NU-ITS/LAPSforMac)
+		pseudoRandom) ## Based on University of Nebraska' LAPS system (https://github.com/NU-ITS/LAPSforMac)
 			NUM=16
 			NEWPASS=$(openssl rand -base64 100 | tr -d OoIi1lLS | head -c${NUM};echo)
+			;;
+
+		custom* | *) ## Adjustable scheme
+			## Example: "schema N=16;S=1;D=2;L=3;U=4"
+
+			## Defaults
+			N=16 # Password length
+			S=1  # Minimum special characters
+			U=1  # Minimum upper case
+			L=1  # Minimum lower case
+			D=1  # Minumum digits
+
+			## If there are overrides passed in, use them
+			INPUT=$(echo ${system} | awk '{print $2}')
+			eval ${INPUT}
+
+			## 33-126 - All the printable characters
+			## 48-57 - Digits
+			## 65-90 - Upper
+			## 97-122 - Lower
+
+			## Generate the minumums
+			UC=($(jot -r ${U} 65 90))  ## Upper case
+			LC=($(jot -r ${L} 97 122)) ## Lower Case
+			NC=($(jot -r ${D} 48 57))  ## Digits
+			## Special characters
+			SCNA=({33..47} {58..64} {91..96} {122..126})
+			SN=()
+			for x in $(jot -r ${S} 0 ${#SCNA[@]}); do
+				SN+=(${SCNA[$x]})
+			done
+
+			## Put the minimums together
+			ALL=(${UC[@]} ${LC[@]} ${NC[@]} ${SN[@]})
+
+			## How many more characters do we need
+			LO=$(($N-$S-$U-$L-$D))
+			## Pull any remaining characters from the whole set
+			if [[ $LO -gt 0 ]]; then
+				for x in $(jot -r $LO 33 126); do
+					ALL+=(${x})
+				done
+			fi
+
+			## Build the password by shuffling the bits
+			passArray=()
+			while [ ${#ALL[@]} -gt 0 ]; do
+				i=$(jot -r 1 0 $(( ${#ALL[@]}-1 )))
+				passArray+=(${ALL[$i]})
+				ALL=( ${ALL[@]/${ALL[$i]}} )
+			done
+			NEWPASS="$(printf '%x' ${passArray[@]} | xxd -r -p)"
+			;;
 
 	esac
 
@@ -149,7 +229,15 @@ function getCurrentPassword() {
 }
 
 function storeCurrentPassword() {
+
 	if [[ ${STORAGE} == "LOCAL" ]]; then
+		## If the file doesn't exist, make it and secure it
+		if [[ ! -f "${LOCALEA}" ]]; then
+			touch "${LOCALEA}"
+			chown root:wheel "${LOCALEA}"
+			chmod 600 "${LOCALEA}"
+		fi
+
 		## Store the password locally for pickup by Recon
 		/usr/bin/defaults write "${LOCALEA}" Password -string "${NEWPASS}"
 	else
@@ -160,9 +248,9 @@ function storeCurrentPassword() {
 	fi
 }
 
-############
-### Main ###
-############
+##
+## Main
+##
 
 ## See if the user exists
 EXISTS=$(id ${ADMINUSER} 2>/dev/null | wc -l | awk '{print $NF}')
